6130ed925786797e010e1abf90e3018c78c3eb0b
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 00:51:19 +0530

    add: post routed

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addPost, getPosts } from '../controllers/postController';
+import { addPost, getPosts, searchPost } from '../controllers/postController';
 
 const router = express.Router();
 
 router.get('/getAllPosts', getPosts);
+router.get('/searchPost', searchPost);
 router.post('/addPost', addPost);
 
 export default router;

950a26a7ea55942603eb7c8f2d1d463d3385d173
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 16:36:42 +0530

    add: show comments

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -18,10 +18,13 @@ import MapsUgcIcon from '@mui/icons-material/MapsUgc';
 import { useDispatch, useSelector } from 'react-redux';
 import { openAddPost } from '../store/reducers/postSlice';
 import { addNewPost } from '../store/actions/postActions';
+import { getAllComments } from '../store/actions/commentActions';
+import Comment from './Comment';
 
 const Post = ({ data }) => {
     const { openAdd } = useSelector((state) => state.post);
     const { userDetails } = useSelector((state) => state.user);
+    const { comments } = useSelector((state) => state.comment);
     const user = window.localStorage.getItem('user');
     const id = window.localStorage.getItem('id');
     console.log(user, userDetails);
@@ -32,6 +35,7 @@ const Post = ({ data }) => {
     };
 
     const [post, setPost] = useState('');
+    const [show, showComments] = useState(null);
 
     const handleAddNewPost = (e) => {
         e.preventDefault();
@@ -39,6 +43,14 @@ const Post = ({ data }) => {
         dispatch(addNewPost({ user: id, username: user, content: post }));
         dispatch(openAddPost(!openAdd));
         window.location.reload();
+    };
+
+    const handleOpenComment = (e, id) => {
+        e.stopPropagation();
+        showComments(id);
+        console.log(id);
+        dispatch(getAllComments(id));
+        console.log(comments);
     };
 
     return (
@@ -68,16 +80,26 @@ const Post = ({ data }) => {
                                 {post.content}
                             </Typography>
                         </CardContent>
+                        <hr />
                         <CardActions
                             sx={{
                                 display: 'flex',
                                 justifyContent: 'flex-end',
                             }}
                         >
-                            <Button size='small'>
+                            <Button
+                                onClick={(e) => handleOpenComment(e, post._id)}
+                                size='small'
+                            >
                                 <MapsUgcIcon />
                             </Button>
                         </CardActions>
+                        {show === post._id && (
+                            <Comment
+                                post={post._id}
+                                data={comments?.docs?.docs}
+                            />
+                        )}
                     </Card>
                 );
             })}

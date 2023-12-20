6415fa8e7bec91c9d322d473249b3c2f4e6b0f93
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 14:46:14 +0530

    add: user add to redux store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import Homepage from './pages/Homepage';
+import { useEffect } from 'react';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from './store/reducers/userSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const getUser = window.localStorage.getItem('user');
+    const userId = window.localStorage.getItem('id');
+    useEffect(() => {
+        console.log(getUser);
+        if (!getUser) {
+            navigate('/login');
+        } else {
+            dispatch(setUserDetails({ user: getUser, id: userId }));
+            navigate('/homepage');
+        }
+    }, []);
+    return (
+        <>
+            {getUser && <Navbar />}
+            <Routes>
+                <Route exact path='/' element={<Homepage />} />
+                <Route exact path='/homepage' element={<Homepage />} />
+                <Route exact path='/login' element={<Signup />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;

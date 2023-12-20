b9d940c5fa1544bda6a34501f044246936781f27
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 15:29:04 +0530

    fix: api call func

--- client/src/store/middleware/userAPI.js
+++ client/src/store/middleware/userAPI.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-export const addUser = (user) => async () => {
+export const addUser = async (user) => {
     try {
-        const data = await axios.post('https://socialsharebackend.onrender.com/api/users/signup', user);
+        const data = await axios.post(
+            'http://localhost:8080/api/user/signup',
+            user
+        );
+        console.log(data);
         return data;
     } catch (error) {
         console.log(error);

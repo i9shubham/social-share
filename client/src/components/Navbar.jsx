90c15388fa243f9bd8d316898426ff28ad08c71a
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 15:27:17 +0530

    add: navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import AppBar from '@mui/material/AppBar';
+import { Box } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+
+// import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import AddIcon from '@mui/icons-material/Add';
+
+import '../App.css';
+import { dispatch } from '../store';
+import { openAddPost } from '../store/reducers/postSlice';
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Navbar = () => {
+    // const [text, setText] = useState('');
+    // const navigate = useNavigate();
+    // const [anchorElNav, setAnchorElNav] = useState(null);
+    // // const [anchorElUser, setAnchorElUser] = useState(null);
+
+    // const handleOpenNavMenu = (event) => {
+    //     setAnchorElNav(event.currentTarget);
+    // };
+    // const handleOpenUserMenu = () => {
+    //     // setAnchorElUser(event.currentTarget);
+    //     navigate('/godfather');
+    // };
+
+    // const handleCloseNavMenu = () => {
+    //     setAnchorElNav(null);
+    // };
+
+    const handleAddPost = () => {
+        console.log('clicked');
+        dispatch(openAddPost(!openAddPost));
+    };
+
+    // const handleCloseUserMenu = () => {
+    //     setAnchorElUser(null);
+    // };
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        console.log(data.get('search'));
+        // console.log(text);
+    };
+
+    return (
+        <AppBar
+            position='fixed'
+            sx={{
+                backgroundColor: '#081863',
+                backdropFilter: 'blur(10px)',
+                borderBottom: '1px solid #182233',
+            }}
+        >
+            <Container maxWidth='xl'>
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title='add post'>
+                            <IconButton onClick={handleAddPost} sx={{ p: 0 }}>
+                                <AddIcon
+                                    sx={{
+                                        color: '#fff',
+                                        width: '15',
+                                        height: '15',
+                                    }}
+                                />
+                            </IconButton>
+                        </Tooltip>
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>
+                        <form onSubmit={(e) => handleSearch(e)} action=''>
+                            <TextField
+                                sx={{
+                                    color: '#fff',
+                                    backgroundColor: '#fff',
+                                    borderRadius: '5px',
+                                }}
+                                name='search'
+                                type='text'
+                                id='standard-basic'
+                                // label='Standard'
+                                placeholder='Search'
+                                variant='standard'
+                                // value={text}
+                                // onChange={(e) => setText(e.target.value)}
+                            />
+                        </form>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Navbar;

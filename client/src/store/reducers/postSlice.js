9803653be79409bd66f4b7031d8b148245bbf16a
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 15:26:41 +0530

    add: getallposts action

--- client/src/store/reducers/postSlice.js
+++ client/src/store/reducers/postSlice.js
@@ -1,22 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addNewPost } from '../actions/postActions';
+import { addNewPost, getAllPosts } from '../actions/postActions';
 
 const initialState = {
     posts: [],
+    openAdd: false,
 };
 
 const postSlice = createSlice({
     name: 'post',
     initialState,
-    reducers: {},
+    reducers: {
+        openAddPost: (state) => {
+            state.openAdd = !state.openAdd;
+        },
+    },
     extraReducers: (builder) => {
         builder.addCase(addNewPost.fulfilled, (state, action) => {
+            state.posts = action.payload;
+        }),
+        builder.addCase(getAllPosts.fulfilled, (state, action) => {
             state.posts = action.payload;
         });
     },
 });
 
-
+export const { openAddPost } = postSlice.actions;
 export const userSelector = (state) => state.post;
 
 export default postSlice.reducer;

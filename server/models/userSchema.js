7420b4a86cf1b05ba8f5bb52d14bdc90258f34e9
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-12-20 11:57:49 +0530

    fix: user schema

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -7,15 +7,6 @@ const User = mongoose.Schema(
             require: true,
             unique: true,
         },
-        name: {
-            type: String,
-            require: true,
-        },
-        email: {
-            type: String,
-            require: true,
-            unique: true,
-        },
         password: {
             type: String,
             require: true,

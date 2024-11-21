cf24cc1779172852022ee3ddf3c32ea8505db9b1
Shubhahm Ingole <ishubham158@gmail.com>
2024-11-21 23:56:26 +0530

    fix: modified authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,28 +3,35 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import Signup from './components/Authentication/Signup';
 import Navbar from './components/Navbar';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUserDetails } from './store/reducers/userSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { setUserDetails } from './store/reducers/userSlice';
 import Login from './components/Authentication/Login';
 import { Container, Grid } from '@mui/material';
+import Menu from './components/Menu';
+import { useEffect, useState } from 'react';
 
 function App() {
-    const isLoading = useSelector(
-        (state) => state.post.isLoading || state.comment.isLoading
+    // const isLoading = useSelector(
+    //     (state) => state.post.isLoading || state.comment.isLoading
+    // );
+    const [user, setUser] = useState(
+        window.localStorage.getItem('user') || null
     );
-    const dispatch = useDispatch();
+
+    // const dispatch = useDispatch();
     const navigate = useNavigate();
-    const getUser = window.localStorage.getItem('user');
-    const userId = window.localStorage.getItem('id');
-    // useEffect(() => {
-    //     console.log(getUser);
-    //     if (!getUser) {
-    //         navigate('/login');
-    //     } else {
-    //         dispatch(setUserDetails({ user: getUser, id: userId }));
-    //         navigate('/homepage');
-    //     }
-    // }, []);
+
+    useEffect(() => {
+        const storedUser = window.localStorage.getItem('user');
+        if (!storedUser) {
+            navigate('/login');
+        } else {
+            setUser(storedUser);
+            // dispatch(setUserDetails({ user: storedUser, id: userId }));
+            navigate('/homepage');
+        }
+    }, [navigate]);
+
     return (
         <Container
             sx={{
@@ -42,11 +49,11 @@ function App() {
                     height: '100vh',
                     overflow: 'auto',
                     '&::-webkit-scrollbar': {
-                      width: '2px'
+                        width: '2px',
                     },
                     '&::-webkit-scrollbar-thumb': {
-                      backgroundColor: '#adb3bd',
-                      borderRadius: '2px'
+                        backgroundColor: '#adb3bd',
+                        borderRadius: '2px',
                     },
                     maxWidth: '400px',
                     // backgroundColor: '#fff',
@@ -55,11 +62,11 @@ function App() {
             >
                 <Grid item xs={12}>
                     <Navbar />
-                    {getUser && isLoading && <p>Loading...</p>}
+                    {user && <Menu />}
                     <Routes>
                         <Route exact path='/' element={<Homepage />} />
                         <Route exact path='/homepage' element={<Homepage />} />
-                        {!getUser && (
+                        {!user && (
                             <>
                                 <Route
                                     exact

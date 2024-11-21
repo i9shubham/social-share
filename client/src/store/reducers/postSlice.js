79d05cc1da3aa6cfaab36d4045eb70a8669ea236
Shubhahm Ingole <ishubham158@gmail.com>
2024-11-22 00:22:08 +0530

    add: posts data

--- client/src/store/reducers/postSlice.js
+++ client/src/store/reducers/postSlice.js
@@ -6,7 +6,80 @@ import {
 } from '../actions/postActions';
 
 const initialState = {
-    posts: [],
+    posts: [
+        {
+            _id: 1,
+            image: 'https://placehold.co/400x400/orange/white?text=i',
+            user: { username: 'i9shubham', bio: 'Owner' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'As the Owner, I am responsible for overseeing all operations and ensuring everything runs smoothly.',
+        },
+        {
+            _id: 2,
+            image: 'https://placehold.co/400x400/orange/white?text=t',
+            user: { username: 'tushar91', bio: 'Doctorate' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'Working as a doctor, I focus on medical science and research.',
+        },
+        {
+            _id: 3,
+            image: 'https://placehold.co/400x400/orange/white?text=b',
+            user: { username: 'stargirl', bio: 'Developer' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'As a Developer, I create and maintain software applications.',
+        },
+        {
+            _id: 4,
+            image: 'https://placehold.co/400x400/orange/white?text=u',
+            user: { username: 'username16', bio: 'Engineer' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'Being an Engineer, I design and build complex systems and structures.',
+        },
+        {
+            _id: 5,
+            image: 'https://placehold.co/400x400/orange/white?text=u',
+            user: { username: 'usernane9', bio: 'Developer' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'As a Developer, I am involved in coding and software development.',
+        },
+        {
+            _id: 6,
+            image: 'https://placehold.co/400x400/orange/white?text=u',
+            user: { username: 'username8', bio: 'Reviewer' },
+            stats: {
+                upvotes: 45,
+                favorites: 32,
+                insights: 20,
+                commentsCount: 36,
+            },
+            post: 'In my role as a Reviewer, I evaluate and provide feedback on various products and services.',
+        },
+    ],
     searchPosts: [],
     openAdd: false,
     isLoading: false,
@@ -18,6 +91,11 @@ const postSlice = createSlice({
     reducers: {
         openAddPost: (state) => {
             state.openAdd = !state.openAdd;
+        },
+        filterPosts: (state, action) => {
+            state.posts = state.posts.filter((post) =>
+                post.post.includes(action.payload)
+            );
         },
     },
     extraReducers: (builder) => {

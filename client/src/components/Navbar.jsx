3e79fc33b9709678890171eb29148ade98b9bbb8
Shubhahm Ingole <ishubham158@gmail.com>
2024-11-21 22:01:48 +0530

    fix: Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,16 @@
-import AppBar from '@mui/material/AppBar';
+import '../App.css';
 import { Box, Card, InputAdornment, Typography } from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
-import Tooltip from '@mui/material/Tooltip';
 import { Search as SearchIcon } from '@mui/icons-material';
-
-// import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
-import AddIcon from '@mui/icons-material/Add';
-
-import '../App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { openAddPost } from '../store/reducers/postSlice';
 import { searchAllPosts } from '../store/actions/postActions';
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = () => {
     const getUser = window.localStorage.getItem('user');
+    console.log(!getUser);
+
     const dispatch = useDispatch();
     const { searchPosts } = useSelector((state) => state.post);
 
@@ -51,7 +44,6 @@ const Navbar = () => {
         const data = new FormData(e.currentTarget);
         console.log(data.get('search'));
         dispatch(searchAllPosts(data.get('search')));
-        // console.log(text);
         console.log(searchPosts);
     };
 
@@ -63,9 +55,9 @@ const Navbar = () => {
                 left: 0,
                 right: 0,
                 width: '100%',
-                maxWidth: '400px !important', // Ensure it has the same max width as the parent
+                maxWidth: '400px !important',
                 backgroundColor: '#004a88',
-                zIndex: 1100, // Ensure it stays above other content
+                zIndex: 1100,
                 padding: '0 16px',
                 margin: '0 auto',
                 textAlign: 'center',
@@ -74,44 +66,13 @@ const Navbar = () => {
         >
             <Typography
                 variant='h5'
-                sx={{ fontWeight: 'bold', color: '#fff', mb: 1, mt: 1}}
+                sx={{ fontWeight: 'bold', color: '#fff', mb: 1, mt: 1 }}
             >
                 Social Share
             </Typography>
-            {/* <Toolbar sx={{ width: '100%' }}> */}
-            {/* <Box sx={{ flexGrow: 0 }}>
-                    <Tooltip title='add post'>
-                        <IconButton onClick={handleAddPost} sx={{ p: 0 }}>
-                            <AddIcon
-                                sx={{
-                                    color: '#fff',
-                                    width: '15',
-                                    height: '15',
-                                }}
-                            />
-                        </IconButton>
-                    </Tooltip>
-                </Box> */}
-            {!getUser && (
+            {getUser && (
                 <Box sx={{ mb: 1 }}>
                     <form onSubmit={(e) => handleSearch(e)} action=''>
-                        {/* <TextField
-                        sx={{
-                            color: '#fff',
-                            backgroundColor: '#fff',
-                            borderRadius: '10px',
-                            width: '100%',
-                            padding: '0 10px',
-                        }}
-                        name='search'
-                        type='text'
-                        id='standard-basic'
-                        // label='Standard'
-                        placeholder='Search topics'
-                        variant='standard'
-                        // value={text}
-                        // onChange={(e) => setText(e.target.value)}
-                    /> */}
                         <TextField
                             size='small'
                             sx={{
@@ -119,7 +80,6 @@ const Navbar = () => {
                                 backgroundColor: '#fff',
                                 borderRadius: '10px',
                                 width: '100%',
-                                // padding: '0 10px',
                             }}
                             id='filled-basic'
                             placeholder='Search topics'
@@ -139,7 +99,6 @@ const Navbar = () => {
                     </div>
                 </Box>
             )}
-            {/* </Toolbar> */}
         </Container>
     );
 };
